6f78410bc894a333e7a88b750377013e87ac6a8e
Michael Fazio <mfazio@skylinetechnologies.com>
2016-11-05 09:44:34 -0500

    Remove/add Nuget files

---
+++ AngryFaceAlertApis/AngryFaceAlertApis/Controllers/ImageController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngryFaceAlertApis.Controllers
+{
+    public class ImageController
+    {
+    }
+}
