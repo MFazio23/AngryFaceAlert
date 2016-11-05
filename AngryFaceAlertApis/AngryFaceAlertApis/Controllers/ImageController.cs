b67882cc88a5b22614b669ac10caff2c3ab7f652
keith czaplerwski <kczaplewski@skylinetechnologies.com>
2016-11-05 16:21:05 -0500

    Find the person

--- AngryFaceAlertApis/AngryFaceAlertApis/Controllers/ImageController.cs
+++ AngryFaceAlertApis/AngryFaceAlertApis/Controllers/ImageController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -14,13 +17,46 @@ namespace AngryFaceAlertApis.Controllers
             Console.WriteLine(image);
             Console.WriteLine(Request.Content);
 
+            var client = new FaceServiceClient("");
+
             var content = Request.Content;
 
-            var byteArray = await content.ReadAsByteArrayAsync();
+            var stream = await content.ReadAsStreamAsync();
 
-            File.WriteAllBytes("C:/dev/Files/test-img.jpg", byteArray);
+            MemoryStream stream2 = new MemoryStream();
 
-            return Ok();
+            stream.CopyTo(stream2);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            var faces = await client.DetectAsync(stream);
+
+            var guids = new List<Guid>();
+
+            foreach(var face in faces)
+            {
+                guids.Add(face.FaceId);
+            }
+
+            await client.TrainPersonGroupAsync("skyline");
+
+            var person = new Person() { Name = "Unknown" };
+
+            try
+            {
+                var possibilities = await client.IdentifyAsync("skyline", guids.ToArray(), 0.5f, 1);
+
+                if (possibilities != null && possibilities.Length > 0)
+                {
+                    person = await client.GetPersonAsync("skyline", possibilities[0].Candidates[0].PersonId);
+                }
+
+   
+            } catch (Exception e)
+            {
+                var stop = "";
+            }
+
+            return Ok(person);
         }
     }
 }
